be8550720010c3c09264b37d808fe1cd3035e9c8
Nilam Bhosale <96847618+BhosaleNilam@users.noreply.github.com>
2024-08-14 17:02:34 -0400

    Add files via upload

---
+++ FirstApplication.java
@@ -0,0 +1,8 @@
+
+public class FirstApplication {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ ReadInput.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadInput {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader
+				(new InputStreamReader(System.in));
+		
+		String name = bf.readLine();
+		System.out.println(name);
+	}
+	
+	
+
+}
